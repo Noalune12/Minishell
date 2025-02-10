0e67a01d49bc3b8add2933a02dad0a21320b3045
Lou-Anne Buisson <louanne.buisson@gmail.com>
2025-02-10 13:52:13 +0100

    test: built-in echo, handle print env variable

--- srcs/temp/built_in/echo.c
+++ srcs/temp/built_in/echo.c
@@ -1,4 +1,4 @@
-#include "../../incs/minishell.h"
+#include "../../../incs/minishell.h"
 
 /*
 COLOR CODE :
@@ -33,7 +33,75 @@ void	get_echo_info(char *input, int *i, int *no_nl)
 	}
 }
 
-void	ft_print_echo(char *input)
+char	*find_var_env(char *var, t_list *env, int len)
+{
+	int		i;
+	t_list	*temp;
+	char	*content;
+
+	i = 0;
+	temp = env;
+	content = NULL;
+	if (!(temp->content))
+		return (NULL);
+	while (temp)
+	{
+		if (strncmp(temp->content, var, len) == 0)
+			{
+				content = temp->content + len + 1;
+				break ;
+			}
+		temp = temp->next;
+	}
+	return (content);
+}
+
+char	*ft_strndup(const char *s, size_t len) // utils
+{
+	char		*str;
+	size_t		i;
+
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+void	print_var(char *input, t_list *env, int *i)
+{
+	char	*var;
+	char	*content;
+	size_t	len;
+	size_t	j;
+
+	var = NULL;
+	len = 0;
+	j = *i + 1;
+	if (!input[*i + 1])
+		return ;
+	else
+		(*i)++;
+	while (input[j] && input[j] != '$' && input[j] != ' ')
+	{
+		len++;
+		j++;
+	}
+	var = ft_strndup(input + *i, len); //protect ?
+	content = find_var_env(var, env, len); // handle in parsing ??
+	if (content)
+		printf("%s", content);
+	(*i) += len - 1;
+	free(var);
+}
+
+void	ft_print_echo(char *input, t_list *env)
 {
 	int	i;
 
@@ -45,12 +113,14 @@ void	ft_print_echo(char *input)
 			while (input[i + 1] && input[i + 1] == ' ') // handle in parsing rm extra space
 			i++;
 		}
-		printf(PURPLE"%c"RESET, input[i]);
+		if (input[i] != '$')
+			printf(PURPLE"%c"RESET, input[i]);
+		else
+			print_var(input, env, &i);
 		i++;
 	}
 }
-
-void	ft_echo(char *input) // if pipe after write in pipe + adapt with struct
+void	ft_echo(char *input, t_list *env) // if pipe after write in pipe + adapt with struct
 {
 	int no_nl = 0;
 	int i = 4;
@@ -65,7 +135,7 @@ void	ft_echo(char *input) // if pipe after write in pipe + adapt with struct
 		printf(YELLOW"echo -n has no agument\n"RESET);
 	while (input[i] && input[i] == ' ')
 		i++;
-	ft_print_echo(input + i);
+	ft_print_echo(input + i, env);
 	if (!no_nl)
 		printf("\n");
 }
