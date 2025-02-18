fafc99b3978c7c9d4af339f8f957c67b505c356f
Lou-Anne Buisson <louanne.buisson@gmail.com>
2025-02-18 09:52:37 +0100

    feat: build the ast tree with pipes, commands, builtins, >, <, >>

--- srcs/ast/ast_utils.c
+++ srcs/ast/ast_utils.c
@@ -1,20 +1,37 @@
 #include "minishell.h"
 
-t_ast	*create_ast_node(t_node_type type, char *content) // debut de creation de larbre, je vais essayer de faire un affichage de larbre correcte avant de commencer a tout parser correctement
-{
-	t_ast	*node;
+// t_ast	*create_ast_node(t_node_type type, char *content) // debut de creation de larbre, je vais essayer de faire un affichage de larbre correcte avant de commencer a tout parser correctement
+// {
+// 	t_ast	*node;
 
-	node = malloc(sizeof(t_ast));
-	if (!node)
-		return (NULL);
-	node->type = type;
-	node->left = NULL;
-	node->right = NULL;
-	if (content) // vraiment utile de check ca ? techniquement tout est != NULL, dans le doute je laisse pour linstant
-		node->content = ft_strdup(content);
-	else
-		node->content = NULL;
-	return (node);
+// 	node = malloc(sizeof(t_ast));
+// 	if (!node)
+// 		return (NULL);
+// 	node->type = type;
+// 	node->left = NULL;
+// 	node->right = NULL;
+// 	if (content) // vraiment utile de check ca ? techniquement tout est != NULL, dans le doute je laisse pour linstant
+// 		node->content = ft_strdup(content);
+// 	else
+// 		node->content = NULL;
+// 	return (node);
+// }
+
+void	ft_free(char **split)
+{
+	size_t	i;
+
+	i = 0;
+	if (split)
+	{
+		while (split[i])
+		{
+			free(split[i]);
+			split[i] = NULL;
+			i++;
+		}
+		free (split);
+	}
 }
 
 void	free_ast(t_ast *node)
@@ -25,7 +42,11 @@ void	free_ast(t_ast *node)
 		free_ast(node->left);
 	if (node->right)
 		free_ast(node->right);
-	if (node->content)
-		free(node->content);
+	if (node->cmd)
+	{
+		free(node->cmd->path);
+		ft_free(node->cmd->cmds);
+		free(node->cmd);
+	}
 	free(node);
 }
