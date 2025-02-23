159f9693237cb0e01a205ea6b62e6889ec073c7e
Lou-Anne Buisson <louanne.buisson@gmail.com>
2025-02-23 13:15:39 +0100

    feat: convert list env to **tab and use it with execve

--- srcs/exec/cmd_exec.c
+++ srcs/exec/cmd_exec.c
@@ -1,12 +1,63 @@
 #include "minishell.h"
 
+static size_t	list_size(t_list *list)
+{
+	t_list	*temp;
+	size_t	size;
+
+	temp = list;
+	size = 0;
+	while (temp)
+	{
+		size++;
+		temp = temp->next;
+	}
+	return (size);
+}
+
+static char	**list_to_tab(t_minishell *minishell)
+{
+	t_list	*temp;
+	char	**tab;
+	size_t	i;
+
+	temp = minishell->envp;
+	tab = (char **)malloc((list_size(temp) + 1) * sizeof(char *));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	while (temp)
+	{
+		tab[i] = ft_strdup(temp->content);
+		if (!tab[i])
+		{
+			while (i > 0)
+				free(tab[--i]);
+			free(tab);
+			return (NULL);
+		}
+		temp = temp->next;
+		i++;
+	}
+	tab[i] = NULL;
+	return (tab);
+}
+
 static int	exec_cmd(t_ast *node, t_minishell *minishell)
 {
+	char	**env;
+
 	if (access(node->cmd->cmds[0], X_OK) == 0)
 		node->cmd->path = ft_strdup(node->cmd->cmds[0]);
 	else
 		node->cmd->path = find_exec_cmd(node->cmd->cmds, minishell, node);
-	execve(node->cmd->path, node->cmd->cmds, NULL); // protect
+	env = list_to_tab(minishell);
+	if (!env)
+	{
+		ft_dprintf(STDERR_FILENO, "Malloc failed\n");
+		// free and exit ??
+	}
+	execve(node->cmd->path, node->cmd->cmds, env); // protect
 	return (1);
 }
 
--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -1,22 +1,6 @@
 #include "minishell.h"
 
-//list to tab
-
 typedef int (* t_handler)(t_ast *node, t_minishell *minishell);
-
-int	update_exit_code(int exit_code, t_minishell *minishell, t_ast *node)
-{
-	static int result;
-
-	if (node->last_branch == 1)
-	{
-		result = exit_code;
-	}
-	printf("result = %d\n", result);
-	minishell->exit_status = result;
-	printf("exit status = %d\n", minishell->exit_status);
-	return (0);
-}
 
 int	exec_minishell(t_ast *node, t_minishell *minishell)
 {
@@ -26,7 +10,7 @@ int	exec_minishell(t_ast *node, t_minishell *minishell)
 	if (!node)
 		return (0);
 	ret = tab[node->type](node, minishell);
-	printf("ret = %d\n", ret);
+	// printf("ret = %d\n", ret);
 	// // else if (node->type == NODE_AND)
 	// // {
 	// // 	if (pipe(minishell->pipe_fd) == -1)
@@ -37,25 +21,5 @@ int	exec_minishell(t_ast *node, t_minishell *minishell)
 	// // 	exec_minishell(node->left, exec, minishell);
 	// // 	exec_minishell(node->right, exec, minishell);
 	// // }
-
-	// // int exit_code;
-	// // waitpid(minishell->pid, &minishell->status, 0);
-	// // if (node->last_branch == 1)
-	// // 	minishell->exit_code = WEXITSTATUS(minishell->status);
-	// // printf("Le code de sortie de %s est : %d\n", node->cmd->cmds[0], minishell->exit_code);
-	// // if (node->last_branch == 1)
-	// // 	return (update_exit_code(minishell->exit_code, minishell, node));
-	// // else
-	// // 	return (0);
-	// if (node->last_branch == 1)
-	// {
-	// 	waitpid(minishell->pid, &minishell->status, 0);
-	// 	minishell->exit_status = WEXITSTATUS(minishell->status);
-	// 	// minishell->exit_code = minishell->exit_status;
-	// 	// return update_exit_code(minishell->exit_code, minishell, node);
-	// }
-	// else
-	// 	waitpid(minishell->pid, NULL, 0);
-	// printf("Le code de sortie de %s est : %d\n", node->cmd->cmds[0], minishell->exit_status);
 	return (ret);
 }
