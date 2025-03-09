4e5f48ee01cc5aa374ec5c531223743523fe3fae
Guillaume Ebersohl <guigui1991@live.fr>
2025-03-09 22:12:45 +0100

    feat: wildcard couting/sorting files recognized functions

--- srcs/wildcard/wildcard_utils.c
+++ srcs/wildcard/wildcard_utils.c
@@ -1,5 +1,7 @@
 #include "minishell.h"
 
+#include <dirent.h>
+
 bool	contains_wildards(char *token)
 {
 	bool	in_squotes;
@@ -22,4 +24,55 @@ bool	contains_wildards(char *token)
 		i++;
 	}
 	return (false);
+}
+
+void	swap_files(char **first_file, char **second_file)
+{
+	char	*tmp;
+
+	tmp = *first_file;
+	*first_file = *second_file;
+	*second_file = tmp;
+}
+
+void	sort_file_names(char **file_names, int count)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < count - 1)
+	{
+		j = i + 1;
+		while (j < count)
+		{
+			if (ft_strcmp(file_names[i], file_names[j]) > 0)
+				swap_files(&file_names[i], &file_names[j]);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	count_matches(char *pattern)
+{
+	DIR				*dir;
+	struct dirent	*entry;
+	int				count;
+
+	count = 0;
+	dir = opendir(".");
+	if (!dir)
+		return (0);
+	entry = readdir(dir);
+	while (entry != NULL)
+	{
+		if (entry->d_name[0] == "." && pattern[0] != ".") // check des fichiers cachés : si le fichier actuel est 
+			continue ;
+		if (wildcard_matches(pattern, entry->d_name))
+			count++;
+		entry = readdir(dir);
+	}
+	closedir(dir);
+	return (count);
 }
