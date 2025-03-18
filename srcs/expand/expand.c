1ce96cc870a43529679a83b2c22b949d18653f2b
Guillaume Ebersohl <guigui1991@live.fr>
2025-03-18 19:14:10 +0100

    fix: fixed expand

--- srcs/expand/expand.c
+++ srcs/expand/expand.c
@@ -20,21 +20,27 @@ static int	handle_dollar_sign(char *str, char *expanded, size_t *i, size_t *j, t
 	return (1);
 }
 
-static int	handle_exit_code(char *expanded, size_t *i, size_t *j, int exit_code)
+static int handle_exit_code(char *expanded, size_t *i, size_t *j, int exit_code)
 {
 	char	*exit_code_str;
+	size_t	k;
 
-	(*i) += 1; // Skip over "$?"
+	(*i) += 1; // Correctly skip over "$?"
 	exit_code_str = ft_itoa(exit_code);
 	if (!exit_code_str)
 		return (0);
-	ft_strcpy(&expanded[*j], exit_code_str);
-	*j += ft_strlen(exit_code_str);
+	k = 0;
+	while (exit_code_str[k])
+	{
+		expanded[*j] = exit_code_str[k];
+		(*j)++;
+		k++;
+	}
 	free(exit_code_str);
 	return (1);
 }
 
-char	*expand_env_vars(char *str, t_list *env, t_minishell *minishell, int *exp, int *quote)
+char *expand_env_vars(char *str, t_list *env, t_minishell *minishell, int *exp, int *quote)
 {
 	size_t	i;
 	size_t	j;
@@ -55,7 +61,37 @@ char	*expand_env_vars(char *str, t_list *env, t_minishell *minishell, int *exp, 
 	{
 		if (!handle_quotes_expand(str[i], &in_squotes, &in_dquotes, quote))
 			expanded[j++] = str[i];
-		else if (str[i] == '$' && str[i + 1] && (str[i + 1] != '?' && ft_isalnum(str[i + 1])))
+		else if (str[i] == '$' && str[i + 1] && str[i + 1] == '"' && !in_dquotes && !in_squotes)
+		{
+			i += 2;
+			while (str[i] && str[i] != '"')
+			{
+				if (str[i] == '$' && str[i + 1] && ft_isalnum(str[i + 1]))
+				{
+					*exp = 1;
+					if (!handle_dollar_sign(str + i, expanded, &i, &j, env))
+					{
+						free(expanded);
+						return (NULL);
+					}
+				}
+				else if (str[i] == '$' && str[i + 1] == '?')
+				{
+					*exp = 1;
+					if (!handle_exit_code(expanded, &i, &j, minishell->exit_status))
+					{
+						free(expanded);
+						return (NULL);
+					}
+				}
+				else
+					expanded[j++] = str[i++];
+			}
+			if (str[i] == '"')
+				i++;
+			i--;
+		}
+		else if (str[i] == '$' && str[i + 1] && ft_isalnum(str[i + 1]))
 		{
 			*exp = 1;
 			if (!handle_dollar_sign(str, expanded, &i, &j, env))
@@ -87,8 +123,6 @@ char	*expand_heredoc(char *str, t_list *env, t_minishell *minishell)
 	size_t	j;
 	char	*expanded;
 	size_t	expanded_len;
-	// bool	in_squotes;
-	// bool	in_dquotes;
 
 	expanded_len = get_expanded_str_len(str, env, minishell);
 	expanded = malloc(sizeof(char) * (expanded_len + 1));
@@ -96,8 +130,6 @@ char	*expand_heredoc(char *str, t_list *env, t_minishell *minishell)
 		return (NULL);
 	i = 0;
 	j = 0;
-	// in_squotes = false;
-	// in_dquotes = false;
 	while (str && str[i])
 	{
 		if (str[i] == '$' && str[i + 1] && (str[i + 1] != '?' && ft_isalnum(str[i + 1])))
