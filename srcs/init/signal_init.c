be3613d5a7c9552165af422a200922aec778d244
Guillaume Ebersohl <guigui1991@live.fr>
2025-03-19 21:02:12 +0100

    fix: handling sigpipe (env | env | env | env)

--- srcs/init/signal_init.c
+++ srcs/init/signal_init.c
@@ -3,33 +3,14 @@
 
 int	g_signal_received = 0;
 
-// void	heredoc_signal_handler(int sig)
-// {
-// 	if (sig == SIGINT)
-// 	{
-// 		g_signal_received = SIGINT;
-// 		rl_replace_line("", 0);
-// 		rl_done = 1;
-
-// 		// close(STDIN_FILENO); // a degager, tres tres mauvaise pratique
-// 	}
-// }
-
 void	signal_handler(int signum) //ctrl c
 {
-	// if (signum == SIGINT)
-	// {
 	rl_done = 1;
-	// printf("\n");
-	// rl_on_new_line();
-	// rl_replace_line("", 0);
-	// rl_redisplay();
 	g_signal_received = signum;
-	// rl_done = 1;
-	// 	g_signal_received = SIGINT;
-	// }
-	// else if (signum == SIGQUIT)
-	// 	g_signal_received = SIGQUIT;
+	if (signum == SIGPIPE)
+	{
+		g_signal_received = SIGPIPE;
+	}
 }
 
 void	handle_signal_main(void)
@@ -37,15 +18,19 @@ void	handle_signal_main(void)
 	struct sigaction	s_sigaction;
 
 	g_signal_received = 0;
+	ft_memset(&s_sigaction, 0, sizeof(s_sigaction));
 	s_sigaction.sa_flags = SA_RESTART;
 	sigemptyset(&s_sigaction.sa_mask);
 	sigaddset(&s_sigaction.sa_mask, SIGINT);
 	sigaddset(&s_sigaction.sa_mask, SIGQUIT);
+	sigaddset(&s_sigaction.sa_mask, SIGPIPE);
 	s_sigaction.sa_handler = signal_handler;
 	sigaction(SIGINT, &s_sigaction, NULL);
 	s_sigaction.sa_handler = SIG_IGN;
 	sigaction(SIGQUIT, &s_sigaction, NULL);
+	sigaction(SIGPIPE, &s_sigaction, NULL);
 }
+
 
 void	signal_handler_exec(int signum)
 {
@@ -75,28 +60,16 @@ void	handle_signal_wait(void)
 	struct sigaction	s_sigaction;
 
 	g_signal_received = 0;
-	s_sigaction.sa_flags = SA_RESTART; // mais pourquoi ??
 	sigemptyset(&s_sigaction.sa_mask);
+	s_sigaction.sa_flags = SA_RESTART; // mais pourquoi ??
 	sigaddset(&s_sigaction.sa_mask, SIGINT);
 	sigaddset(&s_sigaction.sa_mask, SIGQUIT);
+	sigaddset(&s_sigaction.sa_mask, SIGPIPE);
 	s_sigaction.sa_handler = &signal_handler_exec;
 	sigaction(SIGINT, &s_sigaction, NULL);
 	sigaction(SIGQUIT, &s_sigaction, NULL);
+	sigaction(SIGPIPE, &s_sigaction, NULL);
 }
-
-// void	handle_signal_heredoc(void)
-// {
-// 	struct sigaction	s_sigaction;
-
-// 	g_signal_received = 0;
-// 	s_sigaction.sa_flags = SA_RESTART; // mais pourquoi ??
-// 	sigemptyset(&s_sigaction.sa_mask);
-// 	sigaddset(&s_sigaction.sa_mask, SIGINT);
-// 	sigaddset(&s_sigaction.sa_mask, SIGQUIT);
-// 	s_sigaction.sa_handler = &heredoc_signal_handler;
-// 	sigaction(SIGINT, &s_sigaction, NULL);
-// 	s_sigaction.sa_handler = SIG_IGN;
-// 	sigaction(SIGQUIT, &s_sigaction, NULL);}
 
 int	return_global(void)
 {
