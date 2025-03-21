8982ede98da0391c36c339d9c86ac53fd18d09b8
Guillaume Ebersohl <guigui1991@live.fr>
2025-03-21 14:26:54 +0100

    refactor: moved manpanth function to env_update

---
+++ incs/env.h
@@ -0,0 +1,24 @@
+#ifndef ENV_H
+# define ENV_H
+
+# include "types.h"
+
+typedef struct s_shlvl
+{
+	t_list	*shlvl_node;
+	char	*next_lvl;
+	int		shlvl;
+}	t_shlvl;
+
+t_list	*env_init(char **envp);
+t_list	*find_env_node(t_list *env, const char *var_searched);
+t_list	*add_node(t_list **env, char *content);
+
+void	free_list(t_list *list);
+void	update_pwd(t_list **env);
+void	update_shlvl(t_list *env);
+void	add_manpath_to_env(t_list **env);
+
+char	*ft_getenv(char *var_name, t_list *env);
+
+#endif
